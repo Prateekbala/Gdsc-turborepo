e1f287c4e550de4d4d9b6c29ea6feca6552bba21
Prateekbala <145365208+Prateekbala@users.noreply.github.com>
2024-07-30 21:01:06 +0530

    Update page.tsx

--- apps/admin/app/(auth)/sign-in/page.tsx
+++ apps/admin/app/(auth)/sign-in/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+import React, { useRef, useState } from 'react';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import * as z from 'zod';
@@ -11,115 +11,120 @@ import {
   FormLabel,
   FormMessage,
 } from '../../../@/components/ui/form';
-
+import ReCAPTCHA from 'react-google-recaptcha';
 import { Button } from '../../../@/components/ui/button';
 import { Input } from '../../../@/components/ui/input';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useToast } from '../../../@/components/ui/use-toast';
 import { signInSchema } from '../../../schemas/signInSchema';
-import { Loader2 } from 'lucide-react';
-import { useState } from 'react';
 
-  export default function SignInForm() {
-    const [IsSubmitting,setIsSubmitting]=useState(false)
-    const router = useRouter();
-  
-    const form = useForm<z.infer<typeof signInSchema>>({
-      resolver: zodResolver(signInSchema),
-      defaultValues: {
-        identifier: '',
-        password: '',
-      },
-    });
-  
-    const { toast } = useToast();
-    const onSubmit = async (data: z.infer<typeof signInSchema>) => {
-      setIsSubmitting(true)
-      const result = await signIn('credentials', {
-        redirect: false,
-        identifier: data.identifier,
-        password: data.password,
+export default function SignInForm() {
+  const router = useRouter();
+  const recaptchaRef = useRef<ReCAPTCHA>(null);
+  const [recaptchaToken, setRecaptchaToken] = useState<string | null>(null);
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      identifier: '',
+      password: '',
+    },
+  });
+
+  const { toast } = useToast();
+
+  const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+    if (!recaptchaToken) {
+      toast({
+        title: 'Error',
+        description: 'Please complete the reCAPTCHA',
+        variant: 'destructive',
       });
-  
-      if (result?.error) {
-        if (result.error === 'CredentialsSignin') {
-          toast({
-            title: 'Login Failed',
-            description: 'Incorrect username or password',
-            variant: 'destructive',
-          });
-          console.log("error in sign-in ###",result.error)
-        } else {
-          toast({
-            title: 'Error',
-            description: result.error,
-            variant: 'destructive',
-          });
-          console.log("error in sign-in ###",result.error)
-        }
+      return;
+    }
+
+    const result = await signIn('credentials', {
+      redirect: false,
+      identifier: data.identifier,
+      password: data.password,
+      recaptchaToken, 
+    });
+
+    if (result?.error) {
+      if (result.error === 'CredentialsSignin') {
+        toast({
+          title: 'Login Failed',
+          description: 'Incorrect username or password',
+          variant: 'destructive',
+        });
+      } else {
+        toast({
+          title: 'Error',
+          description: result.error,
+          variant: 'destructive',
+        });
       }
-      setIsSubmitting(false)
-      if (result?.url) {
-        router.replace('/dashboard');
-      }
-    };
-  
-    return (
-      <div className="flex justify-center items-center min-h-screen bg-slate-300">
-        <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
-          <div className="text-center">
-            <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
-            ADMIN sign-in
-            </h1>
-            <p className="mb-4">Sign in to continue </p>
-          </div>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-              <FormField
-                name="identifier"
-                control={form.control}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email/Username</FormLabel>
-                    <Input {...field} />
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                name="password"
-                control={form.control}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Password</FormLabel>
-                    <Input type="password" {...field} />
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-                     <Button type="submit" className='w-full' disabled={IsSubmitting}>
-              {IsSubmitting ? (
-                <>
-                  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                  Please wait
-                </>
-              ) : (
-                'Sign IN'
+    }
+
+    if (result?.url) {
+      router.replace('/dashboard');
+    }
+  };
+
+  const onRecaptchaChange = (token: string | null) => {
+    setRecaptchaToken(token);
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-slate-300">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Welcome Back 
+          </h1>
+          <p className="mb-4">Sign in to continue </p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              name="identifier"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email/Username</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
               )}
-            </Button>
-            </form>
-          </Form>
-          <div className="text-center mt-4">
-            <p>
-              Not a member yet?{' '}
-              <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
-                Sign up
-              </Link>
-            </p>
-          </div>
+            />
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <Input type="password" {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <ReCAPTCHA
+              ref={recaptchaRef}
+              sitekey="6LfnBhsqAAAAAEJ5enl1SWXa-HsRAF4ppnTCfTV3"
+              onChange={onRecaptchaChange}
+            />
+            <Button className='w-full' type="submit">Sign In</Button>
+          </form>
+        </Form>
+        <div className="text-center mt-4">
+          <p>
+            Not a member yet?{' '}
+            <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
+              Sign up
+            </Link>
+          </p>
         </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
